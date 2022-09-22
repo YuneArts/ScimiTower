89e19e9768f18bfb46c002931dd779f1160967ff
KTD-FGC <camerontheauthor@gmail.com>
2022-09-21 23:21:43 -0400

    Random Weapon Drops and Weapon Select

--- Assets/Scripts/CameronCode/WeaponSelect.cs
+++ Assets/Scripts/CameronCode/WeaponSelect.cs
@@ -1,22 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WeaponSelect : MonoBehaviour
 {
     public BattleSystem battleSystem;
-    public WeaponDisplay weaponDisplay;
-    private WeaponDataSO weapon;
     public InventoryScript randomWeapon;
+    public InventoryScript playerInventory;
+    public WeaponDataSO theWeapon;
+    public GameObject[] slots;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Pick();
+    }
+
+    void Pick()
+    {
+        int randomIndex = Random.Range(1, randomWeapon.Container.Count);
+        Debug.Log($"{randomIndex}");
+
+        if (randomIndex == 1)
+        {
+            theWeapon = randomWeapon.Container[0];
+        }
+        else if (randomIndex == 2)
+        {
+            theWeapon = randomWeapon.Container[1];
+        }
+        else if (randomIndex == 3)
+        {
+            theWeapon = randomWeapon.Container[2];
+        }
+        else if (randomIndex == 4)
+        {
+            theWeapon = randomWeapon.Container[3];
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (playerInventory.Container.Count == 1)
+        //{
+        //    slots[2].SetActive(false);
+        //    slots[3].SetActive(false);
+            
+        //}
+        //else if (playerInventory.Container.Count == 2)
+        //{
+        //    slots[2].SetActive(true);
+        //    slots[3].SetActive(false);
+        //}
+        //else if (playerInventory.Container.Count == 3)
+        //{
+        //    slots[2].SetActive(true);
+        //    slots[3].SetActive(true);
+        //}
+    }
+
+    public void SlotOne()
+    {
+        playerInventory.Container[0] = theWeapon;
+        SceneManager.LoadScene(1);
         
+    }
+    public void SlotTwo()
+    {
+        playerInventory.Container[1] = theWeapon;
+        SceneManager.LoadScene(1);
+
+    }
+    public void SlotThree()
+    {
+        playerInventory.Container[2] = theWeapon;
+        SceneManager.LoadScene(1);
+
+    }
+    public void SlotFour()
+    {
+        playerInventory.Container[3] = theWeapon;
+        SceneManager.LoadScene(1);
+
     }
 }
