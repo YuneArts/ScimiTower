ff905d67f247892fbf30420f425a81d5a3dc2e6e
JacobRolson <jacobjrolson@gmail.com>
2022-09-27 10:26:05 -0400

    Audio/ Start Button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int gameStart;
+
+    public AudioSource audio;
+
+    public void playButton()
     {
-        
+      audio.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame() 
     {
-        
+      SceneManager.LoadScene("MapScene");
     }
 }
