07aa1d7d95f98408aabbc452a8d032a08f103e84
YuneArts <ThatTastedPurple1@gmail.com>
2022-11-13 18:11:19 -0500

    Changes to Descension Map
    Using new movement system in MapScene. Changes to new map movement script to move within Descension Mode.

--- Assets/Scripts/CameronCode/NewMapMover.cs
+++ Assets/Scripts/CameronCode/NewMapMover.cs
@@ -7,11 +7,17 @@ public class NewMapMover : MonoBehaviour
     public GameObject Player;
     public bool inTransition;
     public int ySpace;
+    public int arrayIndex = 0;
+    public MapSpawn mapSpawnScript;
+    [SerializeField]
+    private TransitionScript sceneTransitionMap;
+
     // Start is called before the first frame update
     void Start()
     {
         inTransition = false;
         ySpace = 3;
+        arrayIndex = DataHolder.Instance.currentIndex;   
     }
 
     // Update is called once per frame
@@ -24,15 +30,31 @@ public class NewMapMover : MonoBehaviour
     {
         if (!inTransition)
         {
-            if (Player.transform.position.y <= this.transform.position.y && this.transform.position.y == Player.transform.position.y + ySpace)
+            if (Player.transform.position.y >= this.transform.position.y && this.transform.position.y == Player.transform.position.y - ySpace)
             {
                 Player.transform.position = this.transform.position;
                 inTransition = true;
+
+                mapSpawnScript.PickEnemy();
+                mapSpawnScript.PickWeapon();
+                mapSpawnScript.PickUpgrades();
+
+                StartCoroutine(SceneManagerAndMovement());
             }
             else
             {
                 Debug.Log("You cannot move here.");
             }
         }
+    }
+
+    IEnumerator SceneManagerAndMovement()
+    {
+        arrayIndex++;
+        //arrayIndex = Mathf.Clamp(arrayIndex, 0, spaces);
+        DataHolder.Instance.currentIndex = arrayIndex;
+        yield return new WaitForSeconds(.5f);
+        sceneTransitionMap.MapToBattleTransition();
+        yield return null;
     }
 }
