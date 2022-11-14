a8abd2484e409273f6dda1652622efcb664f0fe4
YuneArts <ThatTastedPurple1@gmail.com>
2022-11-14 12:34:43 -0500

    Added clamping to map scene camera.

--- Assets/Scripts/MapCamera.cs
+++ Assets/Scripts/MapCamera.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class MapCamera : MonoBehaviour
 {
     [SerializeField]
-    private GameObject mapCamera;
+    private Transform player;
+    //[SerializeField] private Transform camSpot1, camSpot2, camSpot3;
     [SerializeField]
-    private Transform camSpot1, camSpot2, camSpot3;
+    private Vector3 minCameraPos, maxCameraPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,7 @@ public class MapCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (DataHolder.Instance.currentIndex > 2 && DataHolder.Instance.currentIndex < 5)
         {
             mapCamera.transform.position = camSpot2.transform.position;
@@ -29,5 +31,7 @@ public class MapCamera : MonoBehaviour
         {
             mapCamera.transform.position = camSpot1.transform.position;
         }
+        */
+        transform.position = new Vector3(player.position.x, Mathf.Clamp(player.position.y, minCameraPos.y, maxCameraPos.y), transform.position.z);
     }
 }
