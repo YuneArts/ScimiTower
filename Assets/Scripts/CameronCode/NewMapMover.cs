c4f62478dee7160a6f3aa2cd4baa4e1c4e287ba1
YuneArts <ThatTastedPurple1@gmail.com>
2022-11-14 13:47:58 -0500

    Fixed Map not moving to Light/Dark Nodes
    I have no clue what was different.

--- Assets/Scripts/CameronCode/NewMapMover.cs
+++ Assets/Scripts/CameronCode/NewMapMover.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class NewMapMover : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject Player, startingSpot;
     public bool inTransition;
-    public int ySpace;
+    public int ySpace, arrayIndex;
+    public MapSpawn mapSpawnScript;
+    [SerializeField]
+    private TransitionScript sceneTransitionMap;
     public SpriteRenderer node;
     // Start is called before the first frame update
     void Start()
@@ -14,28 +17,50 @@ public class NewMapMover : MonoBehaviour
         inTransition = false;
         ySpace = 3;
         node = GetComponent<SpriteRenderer>();
+        arrayIndex = DataHolder.Instance.currentIndex;
+        if(DataHolder.Instance.currentIndex == 0)
+        {
+            DataHolder.Instance.positionManager.transform.position = startingSpot.transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Player.transform.position = DataHolder.Instance.positionManager.transform.position;
     }
 
     private void OnMouseDown()
     {
         if (!inTransition)
         {
-            if (Player.transform.position.y <= this.transform.position.y && this.transform.position.y == Player.transform.position.y + ySpace)
+            if (Player.transform.position.y >= this.transform.position.y && this.transform.position.y == Player.transform.position.y - ySpace)
             {
                 Player.transform.position = this.transform.position;
+                DataHolder.Instance.positionManager.transform.position = this.transform.position;
                 inTransition = true;
+
+                mapSpawnScript.PickEnemy();
+                mapSpawnScript.PickWeapon();
+                mapSpawnScript.PickUpgrades();
+
+                StartCoroutine(SceneManagerAndMovement());
             }
             else
             {
                 Debug.Log("You cannot move here.");
             }
         }
+    }
+
+    IEnumerator SceneManagerAndMovement()
+    {
+        arrayIndex++;
+        //arrayIndex = Mathf.Clamp(arrayIndex, 0, spaces);
+        DataHolder.Instance.currentIndex = arrayIndex;
+        yield return new WaitForSeconds(.5f);
+        sceneTransitionMap.MapToBattleTransition();
+        yield return null;
     }
 
     private void OnMouseOver()
