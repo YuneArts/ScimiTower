049561f4a27a729ed9a681ceac679bacd3d3934c
KTD-FGC <camerontheauthor@gmail.com>
2022-11-14 17:07:19 -0500

    Show Health Values

--- Assets/Scripts/BattleHUD.cs
+++ Assets/Scripts/BattleHUD.cs
@@ -7,16 +7,26 @@ public class BattleHUD : MonoBehaviour
 {
     public Text nameText;
     public Slider hpSlider;
+    public Text cHP;
+    public Text mHP;
 
     public void SetHUD(Unit unit)
     {
+        unit.currentHP = (int)Mathf.Clamp(unit.maxHP, 0, unit.maxHP);
         nameText.text = unit.cName;
         hpSlider.maxValue = unit.maxHP;
         hpSlider.value = unit.currentHP;
+        cHP.text = unit.currentHP.ToString();
+        mHP.text = unit.maxHP.ToString();
     }
 
     public void SetHP(int hp)
-    {
+    {  
         hpSlider.value = hp;
+        cHP.text = hp.ToString();
+        if (hp < 0)
+        {
+            hp = 0;
+        }
     }
 }
