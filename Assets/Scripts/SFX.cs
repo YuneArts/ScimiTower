dfe46067b2be66b91053dd7728cbe209c3621b18
JacobRolson <jacobjrolson@gmail.com>
2022-11-27 21:40:53 -0500

    SFXSlider

--- Assets/Scripts/SFX.cs
+++ Assets/Scripts/SFX.cs
@@ -8,7 +8,7 @@ public class SFX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        sfx.volume = SettimgsMenu.SFXVOLUME;
     }
 
     // Update is called once per frame
